65d40a8450d9fad33fe0fa13110b7a839bdc456a
OmkarKashid <68742874+OmkarKashid@users.noreply.github.com>
2023-06-12 01:24:33 +0530

    add tab events

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import restaurantHeader from "./restaurantHeader";
 import restaurantDescription from "./restaurantDescription";
 import restaurantNav from "./restaurantNav";
+import menu from "./menu";
+import contact from "./contact";
 
 const content = document.querySelector("#content");
 restaurantHeader(content);
 restaurantNav(content);
 restaurantDescription(content);
+
+function changeTab(tab){
+    switch(tab.dataset.tab){
+        case "Home": restaurantDescription(content);
+                     break;
+        
+        case "Menu": menu(content);
+                     break;
+
+        case "Contact": contact(content);
+                        break;
+
+        default: restaurantDescription(content);
+    }
+}
+
+const tabs = document.querySelectorAll(".navTab");
+tabs.forEach(tab => tab.addEventListener("click", function() {changeTab(tab)}));
